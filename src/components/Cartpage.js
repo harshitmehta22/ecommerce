a5224098575c81118e253461b0191db500e69b4b
A Care indore <acareindore@192.168.1.93>
2023-05-16 14:19:39 +0530

    nf

--- src/components/Cartpage.js
+++ src/components/Cartpage.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 const Cartpage = () => {
-    const {cart,totalQuantity,totalPrice} = useSelector((state) => state.allCart);
+  const { cart, totalQuantity, totalPrice } = useSelector(
+    (state) => state.allCart
+  );
+
   return (
     <>
       <section className="h-100 gradient-custom">
@@ -13,86 +16,85 @@ const Cartpage = () => {
                   <h5 className="mb-0">Cart - 2 items</h5>
                 </div>
                 <div className="card-body">
-                    { cart.map((data) => (
-                       <div className="row">
-                       <div className="col-lg-3 col-md-12 mb-4 mb-lg-0">
-                         <div
-                           className="bg-image hover-overlay hover-zoom ripple rounded"
-                           data-mdb-ripple-color="light"
-                         >
-                           <img
-                             src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
-                             className="w-100"
-                             alt="Blue Jeans Jacket"
-                           />
-                           <a href="#!">
-                             <div
-                               className="mask"
-                               style={{
-                                 backgroundColor: "rgba(251, 251, 251, 0.2)",
-                               }}
-                             ></div>
-                           </a>
-                         </div>
-                       </div>
-   
-                       <div className="col-lg-5 col-md-6 mb-4 mb-lg-0">
-                         <p>
-                           <strong>{data.title}</strong>
-                         </p>
-                         <button
-                           type="button"
-                           className="btn btn-primary btn-sm me-1 mb-2"
-                           data-mdb-toggle="tooltip"
-                           title="Remove item"
-                         >
-                           <i className="fas fa-trash"></i>
-                         </button>
-                       </div>
-   
-                       <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                         <div
-                           className="d-flex mb-4"
-                           style={{ maxWidth: "300px" }}
-                         >
-                           <button
-                             className="btn btn-primary px-3 me-2"
-                             onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                           >
-                             <i className="fas fa-minus"></i>
-                           </button>
-   
-                           <div className="form-outline">
-                             <input
-                               id="form1"
-                               min="0"
-                               name="quantity"
-                               value="1"
-                               type="number"
-                               className="form-control"
-                             />
-                             <label className="form-label" for="form1">
-                               Quantity
-                             </label>
-                           </div>
-   
-                           <button
-                             className="btn btn-primary px-3 ms-2"
-                             onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                           >
-                             <i className="fas fa-plus"></i>
-                           </button>
-                         </div>
-   
-                         <p className="text-start text-md-center">
-                           <strong>$17.99</strong>
-                         </p>
-                       </div>
-                     </div>
-                    ))}
+                  {cart.map((data) => (
+                    <div className="row">
+                      <div className="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                        <div
+                          className="bg-image hover-overlay hover-zoom ripple rounded"
+                          data-mdb-ripple-color="light"
+                        >
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
+                            className="w-100"
+                            alt="Blue Jeans Jacket"
+                          />
+                          <a href="#!">
+                            <div
+                              className="mask"
+                              style={{
+                                backgroundColor: "rgba(251, 251, 251, 0.2)",
+                              }}
+                            ></div>
+                          </a>
+                        </div>
+                      </div>
+
+                      <div className="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                        <p>
+                          <strong>{data.title}</strong>
+                        </p>
+                        <button
+                          type="button"
+                          className="btn btn-primary btn-sm me-1 mb-2"
+                          data-mdb-toggle="tooltip"
+                          title="Remove item"
+                        >
+                          <i className="fas fa-trash"></i>
+                        </button>
+                      </div>
+
+                      <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                        <div
+                          className="d-flex mb-4"
+                          style={{ maxWidth: "300px" }}
+                        >
+                          <button
+                            className="btn btn-primary px-3 me-2"
+                            onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+                          >
+                            <i className="fas fa-minus"></i>
+                          </button>
+
+                          <div className="form-outline">
+                            <input
+                              id="form1"
+                              min="0"
+                              name="quantity"
+                              value="1"
+                              type="number"
+                              className="form-control"
+                            />
+                            <label className="form-label" for="form1">
+                              Quantity
+                            </label>
+                          </div>
+
+                          <button
+                            className="btn btn-primary px-3 ms-2"
+                            onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+                          >
+                            <i className="fas fa-plus"></i>
+                          </button>
+                        </div>
+
+                        <p className="text-start text-md-center">
+                          <strong>$17.99</strong>
+                        </p>
+                      </div>
+                    </div>
+                  ))}
 
                   <hr className="my-4" />
-
                 </div>
               </div>
             </div>
--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -2,29 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 import ProductData from "../components/Productlist";
 
 const initialState = {
-    cart: [],
-    items: ProductData,
-    totalQuantity: 0,
-    totalPrice: 0,
+  cart: [],
+  items: ProductData,
+  totalQuantity: 0,
+  totalPrice: 0,
 };
 
 export const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addToCart: (state,action) =>{
-            let find = state.cart.findIndex((item) => item.id === action.payload.id);
-            if(find >= 0){
-                state.cart[find].quantity += 1;
-            }
-            else{
-                state.cart.push(action.payload);
-            }
-        },
-
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      let find = state.cart.findIndex((item) => item.id === action.payload.id);
+      console.log(find, "fincd");
+      if (find >= 0) {
+        state.cart[find].quantity += 1;
+      } else {
+        state.cart.push(action.payload);
+      }
     },
-})
-export const {addToCart} = cartSlice.actions;
-
+  },
+});
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
